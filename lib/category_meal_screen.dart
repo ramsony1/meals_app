0e5d2647689307e60fcbd182167db40019376228
ramsony1 <62914223+ramsony1@users.noreply.github.com>
2020-06-07 16:22:51 -0400

    adding name routes and passig data using name rout

--- lib/category_meal_screen.dart
+++ lib/category_meal_screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  final String categoryId;
-  final String categoryTitle;
-
-  CategoryMealsScreen(this.categoryId, this.categoryTitle);
-
   @override
   Widget build(BuildContext context) {
+    final routesArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final categoryTitle = routesArgs['title'];
+    final categoryId = routesArgs['id'];
+
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
